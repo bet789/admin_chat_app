c2e3c3f1330a30f62562dc37fc5efca44c0752f4
VillaTaipei <130625766+VillaTaipei@users.noreply.github.com>
2023-04-25 14:46:53 +0700

    Update index.js

--- src/pages/ChatManagement/index.js
+++ src/pages/ChatManagement/index.js
@@ -1,52 +1,27 @@
 import toast, { Toaster } from 'react-hot-toast'
 import axios from 'axios';
-import logo from "../assets/images/taipei101.png";
 import config from "../../helpers/config";
 import { BiEdit } from 'react-icons/bi'
 import { BsTrash } from 'react-icons/bs'
-import { Input, Modal, Table } from 'antd';
-import { useState } from 'react';
+import { Button, Input, Modal, Popconfirm, Switch, Table } from 'antd';
+import { useEffect, useState } from 'react';
 
-const ModalCreate = ({ handleCreateOrUpdateMessage, isOpen, onClose, setNewMessage}) => {
+const ModalCreate = ({ handleCreateOrUpdateMessage, isOpen, onClose, setNewMessage, selectedMessage, newMessage}) => {
     return (
         <Modal open={isOpen} onCancel={onClose} onOk={handleCreateOrUpdateMessage}>
-            <Input type='text' onChange={(e) => setNewMessage(e.target.value)}/>
+            <Input type='text' value={newMessage || selectedMessage?.content} onChange={(e) => setNewMessage(e.target.value)}/>
         </Modal>
     )
 }
 
 const ChatManagement = () => {
-    const [isShowImportExcelModal, setIsShowImportExcelModal] = useState(false)
     const [isShowImportMessageModal, setIsShowImportMessageModal] = useState(false)
-    const [excelFile, setExcelFile] = useState()
     const [messageFile, setMessageFile] = useState()
     const [messageControl, setMessageControl] = useState()
     const [modalCreateMessage, setModalCreateMessage] = useState(false)
     const [messages, setMessages] = useState([])
     const [newMessage, setNewMessage] = useState("")
     const [selectedMessage, setSelectedMessage] = useState()
-
-    const handleImportFileExcel = () => {
-        if (excelFile) {
-            const reader = new FileReader();
-
-            reader.onload = async function (e) {
-                const content = reader.result;
-                // Here the content has been read successfuly
-                const arrayOfUser = content?.toString().split("\r\n").filter(item => item)
-                try {
-                    const importMessage = await axios.post(`${config.api.API_URL_DEV}/file/importUsers`, { users: arrayOfUser })
-
-                    toast.success("Import người dùng thành công")
-                } catch (error) {
-                    console.log(error)
-                    toast.error("Import that bai")
-                }
-            }
-
-            reader.readAsText(excelFile);
-        }
-    }
 
     const getMessageControl = async () => {
         try {
@@ -60,7 +35,8 @@ const ChatManagement = () => {
 
     const getAllFakeMessage = async () => {
         try {
-            axios.get(`${config.api.API_URL_DEV}/api/chat/get-fake-message`, {message: newMessage}).then(res => {
+            axios.get(`${config.api.API_URL_DEV}/api/chat/get-fake-message`).then(res => {
+                console.log(res)
                 setMessages(res)
             })
         } catch (error) {
@@ -78,8 +54,9 @@ const ChatManagement = () => {
                 // Here the content has been read successfuly
                 const arrayOfMessage = content?.toString().split("\r\n").filter(item => item)
                 try {
-                    const importMessage = await axios.post(`${config.api.API_URL_DEV}/file/importMessage`, { messages: arrayOfMessage })
-
+                    const importMessage = await axios.post(`${config.api.API_URL_DEV}/api/file/importMessage`, { messages: arrayOfMessage })
+                    getAllFakeMessage()
+                    setIsShowImportMessageModal(false)
                     toast.success("Import người tin nhắn thành công")
                 } catch (error) {
                     console.log(error)
@@ -101,14 +78,6 @@ const ChatManagement = () => {
         setMessageFile(file)
     }
 
-    const handleChangeFileImport = (file) => {
-        const acceptFileType = ["text/plain"]
-        if (!acceptFileType.includes(file?.type)) {
-            toast.error("Chỉ được nhập vào text")
-            return;
-        }
-        setExcelFile(file)
-    }
 
     const handleChangeMessageControl = async (checked) => {
         try {
@@ -130,28 +99,43 @@ const ChatManagement = () => {
             dataIndex: "_id",
             key: "action",
             render: (row, record) => {
-                return <div><span style={{cursor: 'pointer'}}><BiEdit onClick={()=>{setSelectedMessage(record?._id); setModalCreateMessage(true)}}/></span><span style={{marginLeft: '10px', cursor: 'pointer'}}><BsTrash/></span></div>
+                return <div><span style={{cursor: 'pointer'}}>
+                    <BiEdit onClick={()=>{setSelectedMessage(record); setModalCreateMessage(true)}}/></span><span style={{marginLeft: '10px', cursor: 'pointer'}}></span>
+                    <Popconfirm
+                        placement="topRight"
+                        title={"Bạn có chắc chắn xóa message này?"}
+                        onConfirm={()=>handleDeleteMessage(record._id)}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                    <BsTrash/>
+                    </Popconfirm>
+                    </div>
             }
         }
     ]
+
 
     const createUpdateMessage = async () => {
         try {
             if(selectedMessage){
                 axios.patch(`${config.api.API_URL_DEV}/api/chat/update-fake-message/${selectedMessage?._id || ""}`, {message: newMessage || selectedMessage?.content}).then(res => {
-                    if(res.data?.status == 1){
+                    if(res?.status == 1){
                         toast.success("Cập nhật tin nhắn thành công")
                         getAllFakeMessage()
+                        setModalCreateMessage(false)
                     }
                 })
             }else{
                 axios.post(`${config.api.API_URL_DEV}/api/chat/add-fake-message`, {message: newMessage}).then(res => {
-                    if(res.data?.status == 1){
+                    if(res?.status == 1){
                         toast.success("Tạo tin nhắn thành công")
                         getAllFakeMessage()
+                        setModalCreateMessage(false)
                     }
                 })
             }
+            setSelectedMessage(null)
             
         } catch (error) {
             toast.error("Thất bại")
@@ -160,8 +144,8 @@ const ChatManagement = () => {
 
     const handleDeleteMessage = async (id) => {
         try {
-            axios.delete(`${config.api.API_URL_DEV}/api/chat/delete-fake-message.${id}`).then(res => {
-                if(res.data?.status == 1){
+            axios.delete(`${config.api.API_URL_DEV}/api/chat/delete-fake-message/${id}`).then(res => {
+                if(res.status == 1){
                     toast.success("Xoá tin nhắn thành công")
                     getAllFakeMessage()
                 }
@@ -180,23 +164,17 @@ const ChatManagement = () => {
         <>
             <div>
                 <div>
-                    <Button type="primary" onClick={() => setIsShowImportExcelModal(true)}>Import Fake User</Button>
+                    <Button onClick={()=>{setModalCreateMessage(true)}} type='primary'>Thêm message fake</Button>
                     <Button onClick={() => setIsShowImportMessageModal(true)} style={{ marginLeft: '10px' }} type="primary">Import Fake Messages</Button>
-                    <Switch onChange={(value) => handleChangeMessageControl(value)} checked={messageControl?.auto} style={messageControl?.auto ? { marginLeft: '10px', background: 'green' } : { marginLeft: '10px', background: 'red' }} /><span style={{ color: 'white', marginLeft: '5px' }}>Bật tự động chat</span>
+                    <Switch onChange={(value) => handleChangeMessageControl(value)} checked={messageControl?.auto} style={messageControl?.auto ? { marginLeft: '10px', background: 'green' } : { marginLeft: '10px', background: 'red' }} /><span style={{ color: 'black', marginLeft: '5px' }}>Bật tự động chat</span>
                 </div>
                 <Table
+                    style={{marginTop: '20px'}}
                     pagination
                     columns={colum}
                     dataSource={messages}
                 />
             </div>
-            <Modal
-                open={isShowImportExcelModal}
-                onOk={() => handleImportFileExcel()}
-                onCancel={() => setIsShowImportExcelModal(false)}
-            >
-                <input type='file' onChange={(e) => handleChangeFileImport(e.target?.files?.[0])} />
-            </Modal>
             <Modal
                 open={isShowImportMessageModal}
                 onOk={() => handleImportFileMessage()}
@@ -204,7 +182,7 @@ const ChatManagement = () => {
             >
                 <input type='file' onChange={(e) => handleChangeFileMessage(e.target?.files?.[0])} />
             </Modal>
-            <ModalCreate handleCreateOrUpdateMessage={createUpdateMessage} onClose={setModalCreateMessage} setNewMessage={setNewMessage} isOpen={modalCreateMessage} />
+            <ModalCreate newMessage={newMessage} selectedMessage={selectedMessage} handleCreateOrUpdateMessage={createUpdateMessage} onClose={()=>{setModalCreateMessage(false); setSelectedMessage(null)}} setNewMessage={setNewMessage} isOpen={modalCreateMessage} />
         </>
     )
 }
